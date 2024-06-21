b647003fd790565241e0b64f36d4fd656b2b306d
KevsCha <k-e-v-i-n_13_@hotmail.com>
2024-06-21 13:46:59 +0200

    feat: add files temp js, css and cdn sliders

--- js/gallery.js
+++ js/gallery.js
@@ -1,25 +1,52 @@
+//TODO: leer el json para cargar las imagenes de forma dinamica 
+
 const article_img = document.querySelector('.contain_gallery').querySelectorAll('article');
 const container_elements = document.getElementById('container_elements');
-
 let obj_aside = { 
     'title': container_elements.children[0].children[0].children[0].children[0],
+    'num_img': 0,
+
 };
 
 console.log(obj_aside.title);
 const file_arr = [...article_img];
+// -----------------------function gallery content img---------------
+function aside_modification(item){
+    console.log(item);
+    obj_aside.title.innerText = item.children[1].children[0].innerText;
+}
+// -----------------------function validate clase, remove class visible
+function validate_class(arr){
+    for (const clase of arr) {
+        if (clase === 'no_visible'){
+            container_elements.classList.remove('no_visible');
+            return true;
+        }
+    }
+    return false;
+}
 
+// --------------------------events for articles alls
 for (const box of file_arr) {
     box.addEventListener('click', (event) => {
         const id_item = event.target.parentElement;
-        console.log(id_item)
+        let arr_class = container_elements.className.split(' ');
+        let open_gallery = validate_class(arr_class);
+        if (open_gallery == true){
+            aside_modification(id_item);
+            console.log('ventana abierta');
+        }
+
     });
 }
 
-obj_aside.title.innerText = 'hola';
-console.log(file_arr);
 // -------------------quit_windows img_gallery----------------
 const btn_exit = document.querySelector('.box_quit');
-btn_exit.addEventListener('click', () => {
-    console.log(container_elements);
-    container_elements.classList.add('no_visible');
-});
+if (btn_exit != null){
+
+    btn_exit.addEventListener('click', () => {
+        console.log(container_elements);
+        container_elements.classList.add('no_visible');
+    });
+}
+    
